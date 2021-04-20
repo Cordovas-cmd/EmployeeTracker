5d2cff1e5bfacf1dfebd6ffa4e8e7481240064c6
Samuel Cordova <cordovas1337@gmail.com>
2021-04-19 22:26:57 -0500

    added psuedo code for what I need in prompt

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ const connection = mysql.createConnection({
 });
 
 connection.connect();
+
+//Function to handle data with inquirer prompt
+function runNode() {
+    inquirer.prompt(
+        {
+//View departments, 
+//View all roles
+//view all employees
+// Add department
+// add roles
+// Add employees
+// Delete departments
+//Delete employee
+//Delete roles
+//Update employee Roles
+//Update employee Manager
+//View employee by Manager
+        })
+}
